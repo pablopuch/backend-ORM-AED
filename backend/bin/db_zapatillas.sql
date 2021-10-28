4b4af490d52b1285286f6631ed4da50da8b7f522
pablopuch <pablopuch21@hotmail.com>
2021-10-28 13:03:58 +0100

    in-progress-frontend-ORM

--- backend/bin/db_zapatillas.sql
+++ backend/bin/db_zapatillas.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: db_zapatillas
+-- Host: localhost    Database: db_zapatillas
 -- ------------------------------------------------------
--- Server version	8.0.27
+-- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `clientes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nombre` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,34 @@ INSERT INTO `clientes` VALUES (1,'tere'),(2,'pepe'),(3,'juan'),(7,'carmen');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `compras`
+--
+
+DROP TABLE IF EXISTS `compras`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras` (
+  `idcliente` int NOT NULL,
+  `idzapatillas` int NOT NULL,
+  `cantidad` int NOT NULL,
+  PRIMARY KEY (`idcliente`,`idzapatillas`),
+  KEY `fk_idzapatillas_idx` (`idzapatillas`),
+  CONSTRAINT `fk_idcliente` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_idzapatillas` FOREIGN KEY (`idzapatillas`) REFERENCES `zapatillas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras`
+--
+
+LOCK TABLES `compras` WRITE;
+/*!40000 ALTER TABLE `compras` DISABLE KEYS */;
+INSERT INTO `compras` VALUES (1,1,22),(2,2,44),(3,68,22),(3,70,50),(7,70,5);
+/*!40000 ALTER TABLE `compras` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `empleados`
 --
 
@@ -48,7 +76,7 @@ DROP TABLE IF EXISTS `empleados`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `empleados` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) NOT NULL,
+  `url` varchar(225) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -85,7 +113,7 @@ CREATE TABLE `zapatillas` (
 
 LOCK TABLES `zapatillas` WRITE;
 /*!40000 ALTER TABLE `zapatillas` DISABLE KEYS */;
-INSERT INTO `zapatillas` VALUES (1,'nike',44,10),(2,'vans',41,20),(8,'adidas',42,20),(68,'reebok',45,10),(70,'puma',44,20),(71,'hoka',45,20),(72,'fila',43,30),(73,'joma',44,10),(74,'new balance',44,10),(75,'adidas',44,10),(76,'adidas',43,10),(77,'adidas',42,10),(78,'adidas',41,10),(79,'nike',41,10),(80,'nike',42,10),(81,'nike',44,20);
+INSERT INTO `zapatillas` VALUES (1,'nike',44,10),(2,'vans',41,20),(8,'adidas',42,20),(68,'reebok',45,10),(70,'puma',44,20);
 /*!40000 ALTER TABLE `zapatillas` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -98,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-24 18:12:08
+-- Dump completed on 2021-10-28  8:42:53
